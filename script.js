dad498ca1c4fc54a409c72059952c0dda90c6f05
Victor-Emanuel-Porto <81031972+Victor-Emanuel-Porto@users.noreply.github.com>
2021-11-19 15:07:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ function a() {
     const salario = parseFloat(document.getElementById('v').value);
     const juros = parseFloat(document.getElementById('j').value);
     const parcelas = parseInt(document.getElementById('p').value);
+    const nome = document.getElementById('n').value;
 
     if (salario !== '' && juros !== '' && parcelas !== '')
     {
 
         const total = (salario)*(((1+juros/100)**parcelas)-1) / (juros/100).toFixed(2);
 
-        alert("Total:" + total.toFixed(2));
+        alert( nome+ ", se você aplicar  R$" + salario+ ", à taxa de juros de "+juros+"% ao mês, durante "+parcelas+" meses, acumulará uma poupança de R$"+ total.toFixed(2));
 
 
 
